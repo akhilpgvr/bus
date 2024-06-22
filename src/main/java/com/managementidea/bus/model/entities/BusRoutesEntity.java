24e952498e9f6ee716cde3182c42a2cd70a08033
Akhil <akhilpgvr@gmail.com>
2024-06-22 17:06:31 +0530

    updated businfo and added new entity for routes

--- src/main/java/com/managementidea/bus/model/entities/BusRoutesEntity.java
+++ src/main/java/com/managementidea/bus/model/entities/BusRoutesEntity.java
@@ -1,8 +1,6 @@
 package com.managementidea.bus.model.entities;
 
-import com.managementidea.bus.model.backOffice.EmployeeDetails;
 import com.managementidea.bus.model.backOffice.RouteInfo;
-import com.managementidea.bus.model.enums.BusTypeEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Generated;
@@ -11,21 +9,16 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Set;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "bus-mgmt")
-public class BusInfoEntity {
+@Document(collection = "bus-routes-info")
+public class BusRoutesEntity {
 
     @Id
     @Generated
-    private String id;
+    private String Id;
 
-    private String mobileNo;
     private String busRegNo;
-    private String busRegId;
-    private BusTypeEnum busType;
-    private String Capacity;
-    private EmployeeDetails employeeDetails;
+    private Set<RouteInfo> routeInfo;
 }
